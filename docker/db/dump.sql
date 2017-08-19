d60f63ba3052fb109c681d8fb5230a766debd303
developer <developer@metaps-PN074-no-MacBook-Pro.local>
2017-08-19 19:22:01 +0900

    first commit

---
+++ docker/db/dump.sql
@@ -0,0 +1,42 @@
+--
+-- PostgreSQL database dump
+--
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+
+--
+-- Name: setagaya_db; Type: COMMENT; Schema: -; Owner: setagaya_db
+--
+
+COMMENT ON DATABASE docker IS 'Docker_DB';
+
+
+--
+-- Name: plpgsql; Type: EXTENSION; Schema: -; Owner: 
+--
+
+CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
+
+
+--
+-- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner: 
+--
+
+COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
+
+
+SET search_path = public, pg_catalog;
+
+SET default_tablespace = '';
+
+SET default_with_oids = false;
+
+--
+-- PostgreSQL database dump complete
+--
+
